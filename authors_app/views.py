f02637fed8d4b8027965c96aef182cf41e093af5
ZMaga <m.d.novikova@yandex.ru>
2019-07-16 17:29:53 +0300

    add views methods

--- authors_app/views.py
+++ authors_app/views.py
@@ -1,3 +1,57 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+from .models import Authors
+from .models import Articles
+from .forms import AuthorArticlesForm
 
-# Create your views here.
+
+def article_list(request):
+    articles = Articles.objects.all()
+    return render(request, 'authors_app/article_list.html', {'articles': articles})
+
+
+def article_new(request):
+    if request.method == "POST":
+        form = ArticleForm(request.POST)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.published_date = timezone.now()
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm()
+    return render(request, 'authors_app/article_edit.html', {'form': form})
+
+
+def article_edit(request, pk):
+    article = get_object_or_404(Articles, pk=pk)
+    if request.method == "POST":
+        form = ArticleForm(request.POST, instance=article)
+        if form.is_valid():
+            article = form.save(commit=False)
+            article.published_date = timezone.now()
+            article.save()
+            return redirect('article_detail', pk=article.pk)
+    else:
+        form = ArticleForm(instance=article)
+    return render(request, 'authors_app/article_edit.html', {'form': form})
+
+
+def article_remove(request, pk):
+    # article = get_object_or_404(Articles, pk=pk)
+    article = Articles.objects.get(id=pk)
+    article.delete()
+    # return render(request, 'authors_app/article_remove.html', {'article': article})
+
+
+def article_detail(request, pk):
+    article = get_object_or_404(Articles, pk=pk)
+    return render(request, 'authors_app/article_detail.html', {'article': article})
+
+
+def authors_list(request):
+    authors = Authors.objects.filter(name__contains='а')
+    return render(request, 'authors_app/author_list.html', {'authors': authors})
+
+
+
