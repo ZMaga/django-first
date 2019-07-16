16125f3ba7ea2a49fef6f0434074254ba1dd16be
ZMaga <m.d.novikova@yandex.ru>
2019-07-16 17:27:21 +0300

    change models

--- authors_app/models.py
+++ authors_app/models.py
@@ -4,23 +4,19 @@ from django.utils import timezone
 
 
 class Authors(models.Model):
-    name = models.CharField(default='Some Name', max_length=50, null=False, unique=True)
+    nikname = models.CharField(default='Some Name', max_length=50, null=False, unique=True)
+    name = models.CharField(default='Some Name', max_length=100, null=False)
     birthday = models.DateField(null=False)
 
     def __str__(self):
         return self.name
 
 
-class ArticleAuthor(models.Model):
-    author_id = models.ForeignKey('Authors', on_delete=models.CASCADE)
-    article_id = models.ForeignKey('Articles', on_delete=models.CASCADE)
-#     # TODO: It is need the cascase removing if deleteauthor and he has the article with single author
-
-
 class Articles(models.Model):
+    author = models.ManyToManyField(Authors, through='AuthorArticles')
+    published_date = models.DateTimeField()
     title = models.CharField(max_length=200)
-    published_date = models.DateTimeField(blank=True, null=True)
-    authors = models.ManyToManyField(Authors, through='ArticleAuthor')
+    text = models.TextField()
 
     def publish(self):
         self.published_date = timezone.now()
@@ -28,3 +24,10 @@ class Articles(models.Model):
 
     def __str__(self):
         return self.title
+
+
+class AuthorArticles(models.Model):
+    author_id = models.ForeignKey(Authors, on_delete=models.CASCADE)
+    article_id = models.ForeignKey(Articles, on_delete=models.CASCADE)
+
+
