f649873a6b143bbd037e63607eead98ebaaf6b9e
ZMaga <m.d.novikova@yandex.ru>
2019-07-16 17:37:23 +0300

    add new model

--- authors_app/admin.py
+++ authors_app/admin.py
@@ -1,7 +1,6 @@
 from django.contrib import admin
-from .models import Authors
-from .models import Articles
+from .models import Authors, Articles, AuthorArticles
 
-myModels = [Authors, Articles]
+myModels = [Authors, Articles, AuthorArticles]
 
 admin.site.register(myModels)
