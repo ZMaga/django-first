5e2e467f749ae4844670e21a51ddaee408f8d060
ZMaga <m.d.novikova@yandex.ru>
2019-07-29 03:06:12 +0300

    simple-simple-simple models

--- authors_app/models.py
+++ authors_app/models.py
@@ -1,33 +1,18 @@
-from django.conf import settings
 from django.db import models
-from django.utils import timezone
 
 
 class Authors(models.Model):
-    nikname = models.CharField(default='Some Name', max_length=50, null=False, unique=True)
-    name = models.CharField(default='Some Name', max_length=100, null=False)
-    birthday = models.DateField(null=False)
+    name = models.CharField(max_length=100, null=False)
 
     def __str__(self):
         return self.name
 
 
 class Articles(models.Model):
-    author = models.ManyToManyField(Authors, through='AuthorArticles')
-    published_date = models.DateTimeField()
-    title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200, null=False)
+    # TODO: set not null
     text = models.TextField()
-
-    def publish(self):
-        self.published_date = timezone.now()
-        self.save()
+    authors = models.ManyToManyField(Authors, related_name='articles')
 
     def __str__(self):
         return self.title
-
-
-class AuthorArticles(models.Model):
-    author_id = models.ForeignKey(Authors, on_delete=models.CASCADE)
-    article_id = models.ForeignKey(Articles, on_delete=models.CASCADE)
-
-
